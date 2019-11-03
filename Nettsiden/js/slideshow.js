e20c26010a91aac111aafa8f88f8de6c6a6ecf78
jennylun <jenny.lunde@live.no>
2019-11-03 16:31:52 +0100

    Laget slideshow på index.html

--- Nettsiden/js/slideshow.js
+++ Nettsiden/js/slideshow.js
@@ -1,10 +1,6 @@
 var slideIndex = 0;
 showSlides();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
 // Thumbnail image controls
 function currentSlide(n) {
@@ -22,7 +18,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 10 seconds
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
 
 //Code from https://www.w3schools.com/howto/howto_js_slideshow.asp
