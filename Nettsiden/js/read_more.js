90b7886693c8ea58a671a6a31043359f10c5a65c
jennylun <jenny.lunde@live.no>
2019-11-03 17:57:46 +0100

    Mer på kalender og lagt til read_more.js
    Read_more.js er til kapreferater for å ha en les mer funksjon.

---
+++ Nettsiden/js/read_more.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Code from https://www.w3schools.com/howto/howto_js_read_more.asp
