451fe6f110545c6c96283992abe4971f023ac0bd
andreegeli <andreegeli@gmail.com>
2019-11-04 21:57:36 +0100

    Jeg og petter har jobbet nå ja

--- Nettsiden/js/include.js
+++ Nettsiden/js/include.js
@@ -2,55 +2,55 @@
 
 
 
-const navbar_content = <div id="nav">\
-      <nav>\
-        <ul style="list-style: none;">\
-        <li class='dropdown'>\
-          <a href='javascript:void(0)' class='dropdown-button'>Kamper</a>\
-          <div class='dropdown-content card'>\
-            <a href='kalender.html'>Kalender</a>\
-            <a href='kamper.html'>Referater</a>\
-          </div>\
-        </li>\
-          <li><a href="tabell.html">Tabell</a></li>\
-          <li class='dropdown'>\
-            <a href='javascript:void(0)' class='dropdown-button'>Laget</a>\
-            <div class='dropdown-content card'>\
-              <a href='spillere.html'>Spillere</a>\
-              <a href='stab.html'>Stab</a>\
-            </div>\
-          </li>\
-          <li><a href="Sosialt.html">Sosialt</a></li>\
-          <li onclick="openForm()">Kontakt\
-            <div class="form-popup" id="myForm">\
-              <form class="form-container" action="javascript:void(0);">\
-                <span class="close-btn" onclick="closeForm()">&times;</span>\
-                <h1>Send mail!</h1>\
+const navbar_content = "<div class='nav'>\
+          <nav>\
+            <ul style='list-style: none;'>\
+            <li class='dropdown'>\
+              <a href='javascript:void(0)' class='dropdown-button' onclick='myFunction()'>Kamper</a>\
+              <div class='dropdown-content card'>\
+                <a href='kalender.html'>Kalender</a>\
+                <a href='kampreferat.html'>Referater</a>\
+              </div>\
+            </li>\
+              <li><a href='tabell.html'>Tabell</a></li>\
+              <li class='dropdown'>\
+                <a href='javascript:void(0)' class='dropdown-button' onclick='myFunction()'>Laget</a>\
+                <div class='dropdown-content card'>\
+                  <a href='spillere.html'>Spillere</a>\
+                  <a href='stab.html'>Stab</a>\
+                </div>\
+              </li>\
+              <li><a href='Sosialt.html'>Sosialt</a></li>\
+              <li onclick='openForm()'>Kontakt\
+                <div class='form-popup' id='myForm'>\
+                  <form class='form-container' action='javascript:void(0);'>\
+                    <p onclick='closeForm()' class='close-btn'>&times;</p>\
+                    <h1>Send mail!</h1>\
 
-                <label for="email"><b>Email</b></label>\
-                <input type="email" placeholder="Skriv Email" name="email" required>\
-                <br>\
+                    <label><b>Email</b></label>\
+                    <input type='email' placeholder='Skriv Email' name='email' required>\
+                    <br>\
 
-                <label for="navn"><b>Navn</b></label>\
-                <input type="text" placeholder="Skriv navn" name="text" required>\
-                <br>\
+                    <label><b>Navn</b></label>\
+                    <input type='text' placeholder='Skriv navn' name='text' required>\
+                    <br>\
 
-                <label for="om"><b>Hvem er du?</b></label>\
-                <input type="text" placeholder="Skriv litt om hvorfor du tar kontakt..." name="tekst">\
-                <br>\
+                    <label><b>Hvem er du?</b></label>\
+                    <input type='text' placeholder='Skriv litt om hvorfor du tar kontakt...' name='tekst'>\
+                    <br>\
 
-                <button type="submit" class="btn">Send inn</button>\
-              </form>\
-            </div>\
-          </li>\
-          <li><a href="om.html">Om oss</a></li>\
-        </ul>\
-      </nav>\
-    </div>;
+                    <button type='submit' class='btn'>Send inn</button>\
+                  </form>\
+                </div>\
+              </li>\
+              <li><a href='om.html'>Om oss</a></li>\
+            </ul>\
+          </nav>\
+        </div>";
 
 
 /* Save the footer content in a constant */
-const footer_content = '<footer id="footer">\
+const footer_content = '<footer id='footer'>\
 
 </footer>';
 
--- Nettsiden/js/resize_pic.js
+++ Nettsiden/js/resize_pic.js
@@ -1,9 +1,9 @@
 function large(x){
-  var y=x.width*2;
+  var y=x.width*1.5;
   x.style.width=y+'px';
 }
 
 function tilbake(x){
-  var y=x.width*0.5;
+  var y=x.width*(2/3);
   x.style.width=y+'px';
 }
