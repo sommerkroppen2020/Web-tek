06c544f48cd103a79265bfc9c0065da1b40577e5
jennylun <jenny.lunde@live.no>
2019-11-07 12:07:31 +0100

    Endring av fargene på nav-baren

---
+++ prosjekt3/script/slideshow.js
@@ -0,0 +1,28 @@
+var slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+//automatic slideshow
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 10 seconds
+}
+
+//Code from https://www.w3schools.com/howto/howto_js_slideshow.asp
