36e19dcd610544c55e595de7ffd7339e3cd1a27c
andreegeli <andreegeli@gmail.com>
2019-11-08 10:33:36 +0100

    footer er good

--- Nettsiden/js/include.js
+++ Nettsiden/js/include.js
@@ -31,17 +31,17 @@ const navbar_content = "<div class='navbar'>\
 
 /* Save the footer content in a constant */
 const footer_content = '<footer id="footer">\
-  <div class="auto-margin" id="footer-container">\
+  <div id="footer-container">\
     <div class="row">\
         <div class="col-3"><p>Realkameratene FK</p></div>\
-        <div class="col-3"><p><a target="_blank" href="">Tempe Arena</a></p></div>\
+        <div class="col-3"><p>Tempe Arena</p></div>\
         <div class="col-3"><p>Telefonnummer: <br>+47 472 56 744</p></div>\
         <div class="col-3"><p><a target="_top" href="mailto:johannes.padel@gmail.com">johannes.padel@gmail.com</a></p></div>\
     </div>\
-  </div>\
-  <div class="auto-margin" id="footer-container-liten-skrift">\
-    <hr>\
-    <p id="liten-skrift">This site is constantly reviewed to avoid errors, but we cannot warrant full correctness of all content and/or pricing. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 2017-2017 by Trondheim Hostel Holding AS &copy;&reg;. All Rights Reserved. This webspace is created by Martin Egeli, Mathias Skotnes, Mats Kvelland, Anders Bergesen og Lars Lien Ankile.</p>\
+    <div id="footer-container-liten-skrift">\
+      <hr>\
+      <p id="liten-skrift">While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 2019 by Realkameratene FK. All Rights Reserved. This webspace is created by André Egeli, Sander Ruud, Inger Yri, Jenny Lunde & Petter Giørtz.</p>\
+    </div>\
   </div>\
 </footer>';
 
